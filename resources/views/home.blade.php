e41ec5b1d3443f2b57932d9e21b31edb001508e2
Gelmar0919 <deguzman.gelmar@isu.edu.ph>
2022-11-22 21:20:27 +0800

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,15 +43,11 @@
     
     
     <section class="u-backlink u-clearfix u-grey-80">
-      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
-        <span>Website Templates</span>
-      </a>
-      <p class="u-text">
-        <span>created with</span>
-      </p>
-      <a class="u-link" href="" target="_blank">
-        <span>Website Builder Software</span>
-      </a>. 
-    </section>
+      
+        <p class="u-text" style="color: rgb(75, 75, 75)">
+              &copy; Copyright 2022
+        </p>
+         
+      </section>
   
 </body></html>
